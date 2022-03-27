6f99d501aa36793a90bcf0f5d3e8c81faf573f3f
mandeep <mandeepnain84@gmail.com>
2022-03-27 20:23:17 +0530

    This is first git

---
+++ gittest.java
@@ -0,0 +1,15 @@
+public class gittest {
+
+    static int sum(int x, int y){
+        return x+y;
+    }
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("This is working fine");
+        int sum = sum(2,3);
+        System.out.println(sum);
+    }
+}
